ec7e6ae96964d9ab8e24ae1ff46cac65399b3e13
ozen-hub <info.ozenhub@gmail.com>
2022-12-17 11:20:18 +0530

    2

--- src/ComboBoxFormController.java
+++ src/ComboBoxFormController.java
@@ -1,10 +1,20 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
 public class ComboBoxFormController {
     public TextField txtInputLanguage;
-    public ComboBox<String> cmbLanguageList; // Object => String
+    public ComboBox<String> cmbLanguageList;
+
+    public void initialize(){
+        ObservableList<String> obList = FXCollections.observableArrayList();
+        obList.addAll("Java","Php","C++","Rust","Ruby","Dart");
+        cmbLanguageList.setItems(obList);
+    }
 
     public void btnAddItemOnAction(ActionEvent actionEvent) {
         cmbLanguageList.getItems().add(txtInputLanguage.getText());
