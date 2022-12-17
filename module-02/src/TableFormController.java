f224d8d755df27d831ba260dde76430ee41e7add
ozen-hub <info.ozenhub@gmail.com>
2022-12-17 12:43:14 +0530

    9

--- module-02/src/TableFormController.java
+++ module-02/src/TableFormController.java
@@ -1,16 +1,15 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.util.Optional;
 
 
 public class TableFormController {
 
-    public void initialize(){
+    public void initialize() {
 
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
         colName.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -22,11 +21,25 @@ public class TableFormController {
     }
 
     private void loadCustomers() {
-        ObservableList<CustomerTm> tmList= FXCollections.observableArrayList();
-        for (Customer c:Db.customerTable
-             ) {
-            Button btn= new Button("Delete");
-            CustomerTm tm= new CustomerTm(c.getId(),c.getName(),c.getAddress(),c.getSalary(),btn);
+        ObservableList<CustomerTm> tmList = FXCollections.observableArrayList();
+        for (Customer c : Db.customerTable
+        ) {
+            Button btn = new Button("Delete");
+            CustomerTm tm = new CustomerTm(c.getId(), c.getName(), c.getAddress(), c.getSalary(), btn);
+
+            btn.setOnAction((e) -> {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                        "Are you sure?", ButtonType.YES, ButtonType.NO);
+                //alert.show();
+                Optional<ButtonType> type =alert.showAndWait();
+                if (type.get()==ButtonType.YES){
+                    Db.customerTable.remove(c);
+                    new Alert(Alert.AlertType.INFORMATION, "Deleted!").show();
+                    loadCustomers();
+                }
+
+            });
+
             tmList.add(tm);
         }
         tblCustomer.setItems(tmList);
