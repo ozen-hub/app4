df9a54bb505f4010d113ffcc2c182b40071b87e0
ozen-hub <info.ozenhub@gmail.com>
2022-12-17 10:59:29 +0530

    1

--- src/ComboBoxFormController.java
+++ src/ComboBoxFormController.java
@@ -1,2 +1,14 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class ComboBoxFormController {
+    public TextField txtInputLanguage;
+    public ComboBox<String> cmbLanguageList; // Object => String
+
+    public void btnAddItemOnAction(ActionEvent actionEvent) {
+        cmbLanguageList.getItems().add(txtInputLanguage.getText());
+        txtInputLanguage.clear();
+        txtInputLanguage.requestFocus();
+    }
 }
